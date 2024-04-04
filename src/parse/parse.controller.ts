1664ef5e05b45adbbbadc6850c08d60f73b68448
Konstantin Barbin <konstantin.barbin@charge-amps.com>
2024-04-04 22:19:09 +0300

    Types were added

--- src/parse/parse.controller.ts
+++ src/parse/parse.controller.ts
@@ -1,19 +1,20 @@
 import { Controller, Get } from '@nestjs/common'
 import { ParseService } from './parse.service'
+import { Movie, Tab } from './types'
 
 @Controller('parse')
 export class ParseController {
     constructor(private readonly parseService: ParseService) {}
 
     @Get('/')
-    async fetchTabs(): Promise<string> {
+    async fetchTabs(): Promise<Tab[]> {
         const html = await this.parseService.fetchContent()
         const parsedData = this.parseService.parseTabs(html)
         return parsedData
     }
 
     @Get('/movies')
-    async fetchMovies(): Promise<string> {
+    async fetchMovies(): Promise<Movie[]> {
         const html = await this.parseService.fetchContent('https://uaserial.club/movie')
         const moviesJSON = this.parseService.parseMovies(html)
         return moviesJSON
---
+++ src/parse/types/index.ts
@@ -0,0 +1,10 @@
+export type Movie = {
+    title: string
+    year: string
+    genres: string[]
+    image: string
+}
+
+export type Tab = {
+    title: string
+}
